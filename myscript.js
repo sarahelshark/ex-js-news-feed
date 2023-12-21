adfb257f692140c96f3eb2c83e95d47ad29e3123
sarahelshark <sarahelsh183@gmail.com>
2023-12-21 09:32:40 +0100

    feat add
    scrollbar-not definitive

--- myscript.js
+++ myscript.js
@@ -1,1 +1,8 @@
 console.log('hello');
+//scrollbar che si vede solo allo scroll
+//inserire dinamicamente in pagina i post
+//inserire select dinamicamente in pagina
+//programma filtro dei post
+//programma post salvati
+//style dettagli
+//date method
