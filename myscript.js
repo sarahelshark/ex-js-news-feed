31354ab5796c1690002153dd63a63ba76c3094de
sarahelshark <sarahelsh183@gmail.com>
2023-12-27 16:52:51 +0100

    feat add
    array of objects

--- myscript.js
+++ myscript.js
@@ -8,34 +8,80 @@ console.log('hello');
 //style dettagli
 //date method
 
-/*bookmark che cambia al click
+/*bookmark che cambia al click--step4, ragiono dopo
 -------------------------------
 -creo una array vuota per inserirci i miei click di bookmark
 -procedo con un event listener sui bookmark
   -al click, devono cambiare aspetto
   -pusho il click dentro il mio arrai che si userà dopo per creare il filtro dei post salvati 
-
-
-*/
-
  //Selezione degli elementi di bookmark sia solidi che regolari
 const solidBookmark = document.querySelector('.fa-solid.fa-bookmark');
 const regularBookmark = document.querySelector('.fa-regular.fa-bookmark'); 
 
-
 // Seleziona tutti gli elementi di bookmark, sia solidi che regolari
 const allBookmarks = document.querySelectorAll('.fa-bookmark');
 
+
 // Itera su tutti gli elementi di bookmark e aggiungi l'event listener
 allBookmarks.forEach(bookmark => {
+    
     bookmark.addEventListener('click', function () {
-        // Tuo codice qui per l'evento di clic sul bookmark
+        // 
         console.log('Bookmark Clicked');
         // Add a class
         solidBookmark.classList.toggle('d-none'); 
         regularBookmark.classList.toggle('d-none');
+
            
     });
 });
 
 
+*/
+
+//STEP1- stampo tutti gli articoli in pagina
+//creo una lista di oggetti
+const allArticles = [
+  {
+      title: 'Scoperta di una nuova specie di papera di gomma',
+      content: 'Un breve articolo sulla recente scoperta di una specie di papera di gomma mai vista prima.',
+      tags: ['geo', 'tech'],
+      author: 'Diana Rossi',
+      published: new Date('2023-02-11'),
+      image: 'rubber-duck.jpg',
+      alt: 'rubber duck in a river',
+      id: '1'
+  },
+  {
+      title: 'Esplorando le profondità marine: il mistero degli abissi',
+      content: "un viaggio nelle profondità dell'oceano alla scoperta di creature misteriose e inesplorate",
+      tags: ['viaggi', 'geo'],
+      author: 'Fabio Mari',
+      published: new Date('2023-03-14'),
+      image: 'deep-sea.jpg',
+      alt: 'deep sea',
+      id: '2'       
+  },
+  {
+      title: 'Viaggio culinario: alla ricerca dei sapori perduti',
+      content: 'Esplorazione di tradizioni culinarie dimenticate e la ricerca di sapori autentici.',
+      tags: ['cucina'],
+      author: 'Marta Bianchi',
+      published: new Date('2023-04-20'),
+      image: 'kitchen-food.jpg',
+      alt: 'a table with tomatoes, a knife in the middle and some pot herbs',
+      id: '3'
+  },
+  {
+      title: 'Arte moderna: oltre i confini convenzionali',
+      content: "Un'analisi delle tendenze e delle sfide nell'arte contemporanea, con interviste a artisti emergenti.",
+      tags: ['arte', 'tech'],
+      author: 'Gabriele Neri',
+      published: new Date('2023-05-29'),
+      image: 'modern-art.jpg',
+      alt: 'wall full of graffiti, street art',
+      id: '4'
+  },
+]
+
+//console.log(allArticles);
